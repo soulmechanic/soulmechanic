747727a3835e2aa7e2694742cb1bfb6740e328e2
Avinash Gopinath <69844824+soulmechanic@users.noreply.github.com>
2023-05-30 13:52:50 +0530

    Create reorder_dataframe_columns.py

---
+++ reorder_dataframe_columns.py
@@ -0,0 +1,33 @@
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+import json
+import requests
+import io
+  
+def reorder_dataframe_columns(snowflake_connection, table_name, schema, df):
+    """ Reorder columns in a Pandas DataFrame based on the order of columns in a Snowflake table.
+
+        :param snowflake_connection: A valid Snowflake connection object
+        :param table_name: The name of the Snowflake table to use as reference
+        :param schema: The name of the Snowflake schema to use as refrence
+        :param df: The Pandas DataFrame to reorder columns
+        :return: A new Pandas DataFrame with reordered columns
+        """
+    try:
+        # Get the column names from the Snowflake table
+        result = snowflake_connection.cursor().execute(f"DESCRIBE TABLE {schema}.{table_name}")
+        columns = [row[0] for row in result]
+
+        # Check if any columns are missing in the DataFrame
+        missing_columns = [col for col in columns if col not in df.columns]
+        if missing_columns:
+            raise ValueError(f"The following columns are missing in the DataFrame: {missing_columns}")
+
+        # Reorder the columns in the DataFrame
+        df = df[columns]
+
+        return df
+    except Exception as e:
+        print(f"An error occurred while reordering columns: {e}")
+        return df
